caf88c058c04890863c4073a392c4709fdb00ff6
Arkeil <kohwj1@gmail.com>
2025-06-24 15:23:48 +0900

    디버깅용 정답 콘솔 출력 주석 처리

--- 3.js/js/number_guess.js
+++ 3.js/js/number_guess.js
@@ -2,7 +2,7 @@ const answer = Math.floor(Math.random() * 101);
 const submit = document.getElementById('submit');
 const history = document.getElementById('history');
 
-console.log(answer)
+// console.log(answer)
 
 submit.onclick = () => {
     checkAnswer();
@@ -10,6 +10,13 @@ submit.onclick = () => {
 
 function checkAnswer() {
     const input = document.getElementById('input').value;
+
+    if (input <=0 || input > 100) {
+        alert('1~100 사이의 자연수를 입력해 주세요!!')
+        //자연수 체크 어떻게...? 소숫점 입력받고 싶지 않음..
+        return;
+    }
+
     const history_line = document.createElement('li');
     
     history_line.innerHTML = `예측 숫자: ${input}`;
