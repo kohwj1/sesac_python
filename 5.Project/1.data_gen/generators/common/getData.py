e1f3722817ae687ded49e14ee5cc58316b9e8fa2
Arkeil <kohwj1@gmail.com>
2025-07-04 13:45:22 +0900

    removed exception pass logic

--- 5.Project/1.data_gen/generators/common/getData.py
+++ 5.Project/1.data_gen/generators/common/getData.py
@@ -2,17 +2,16 @@ import pandas
 import random
 
 class GetData:
-    def get_uuid(self, type:str) -> list:
-        if type not in  ['user', 'store', 'item', 'order']:
+    def get_uuid(self, table_type:str) -> list:
+        if table_type not in ['user', 'store', 'item', 'order']:
             print('추출할 수 없는 타입입니다.')
         else:
             try: 
-                file = pandas.read_csv(f'export/{type}.csv')
+                file = pandas.read_csv(f'output/{table_type}.csv')
                 uuid_list = file['Id']
                 return list(uuid_list)
             except FileNotFoundError:
-                # print('csv 파일이 없습니다. 해당 csv가 생성되어 있는지 확인해주세요.')
-                pass #예외처리 대응 필요
+                print('csv 파일이 없습니다. 해당 csv가 생성되어 있는지 확인해주세요.')
     
     def get_txt(self, file_path:str) -> list:
         try:
@@ -20,7 +19,7 @@ class GetData:
                 data = file.read().splitlines()
             return data
         except FileNotFoundError:
-            pass #예외처리 대응 필요
+                print('txt 파일이 없습니다. 경로를 다시 확인해주세요.')
     
     def get_rand(self, file_path) -> str:
         return random.choice(self.get_txt(file_path))
