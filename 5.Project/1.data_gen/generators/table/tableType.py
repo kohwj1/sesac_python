911f397c8f765a703a185579177661d3e0689f3d
Arkeil <kohwj1@gmail.com>
2025-07-04 14:28:00 +0900

    added TableType class

---
+++ 5.Project/1.data_gen/generators/table/tableType.py
@@ -0,0 +1,16 @@
+from generators.table.user import User
+from generators.table.store import Store
+from generators.table.item import Item
+from generators.table.order import Order
+from generators.table.orderItem import OrderItem
+
+class TableType():
+    instance = {'user':(['Id','Name','Gender','Age','Birthdate','Address'],User()),
+                'store':(['Id','Name','Type','Address'],Store()),
+                'item':(['Id','Name','Type','UnitPrice'],Item()),
+                'order':(['Id','OrderAt','StoreId','UserId'],Order()),
+                'orderitem': ([['Id','OrderId','ItemId']],OrderItem())
+                }
+    @classmethod    
+    def add_type(cls,key,value):
+        cls.instance[key] = value
