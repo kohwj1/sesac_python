6b150962597ab68b35da6239def48f6f5a87de47
Arkeil <kohwj1@gmail.com>
2025-07-07 21:14:48 +0900

    improved query param check

--- 6.flask/2.template/5.app.py
+++ 6.flask/2.template/5.app.py
@@ -11,20 +11,37 @@ users = [
 ]
 
 #이름/나이로 검색하기
+# @app.route('/')
+# def user_search():
+#     name = request.args.get('username')
+#     age = request.args.get('age')
+#     result = users
+
+#     if name and age:
+#         result = [u for u in users if (u['name'].lower() == name.lower()) and (u['age'] == int(age))]
+#     elif name:
+#         result = [u for u in users if (u['name'].lower() == name.lower())]
+#     elif age:
+#         result = [u for u in users if (u['age'] == int(age))]
+    
+#     return render_template('index5.html', users=result)
 @app.route('/')
 def user_search():
     name = request.args.get('username')
     age = request.args.get('age')
-    result = users
+    phone = request.args.get('phone')
+    user_filter = users
 
-    if name and age:
-        result = [u for u in users if (u['name'].lower() == name.lower()) and (u['age'] == int(age))]
-    elif name:
-        result = [u for u in users if (u['name'].lower() == name.lower())]
-    elif age:
-        result = [u for u in users if (u['age'] == int(age))]
+    if name:
+        user_filter = [u for u in user_filter if u['name'].lower() == name.lower()]
     
-    return render_template('index5.html', users=result)
+    if age:
+        user_filter = [u for u in user_filter if u['age'] == int(age)]
+    
+    if phone:
+        user_filter = [u for u in user_filter if u['mobile'].replace('-','') == phone.replace('-','')]
+    
+    return render_template('index5.html', users=user_filter)
 
 if __name__ == '__main__':
     app.run(debug=True)
