20a4f9a535abcae8415dedd6bb33527a28235537
Arkeil <kohwj1@gmail.com>
2025-07-08 15:05:01 +0900

    flask server config drill

---
+++ 6.flask/2.template/13.app_config.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, render_template, redirect, url_for
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_FILE_EXT'] = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024 #서버에서 정의한 용량 제한 --> Flask가 알아서 해줌
+
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True) #폴더 생성 (있어도 허용)
+
+def get_filelist():
+    filelist = os.listdir(app.config['UPLOAD_FOLDER'])
+    return filelist
+
+# def get_filesize(file):
+#     pos = file.stream.tell() #현재 포인터 기록
+#     file.stream.seek(0, os.SEEK_END) #포인터 끝까지 이동 (용량 측정)
+#     size = file.stream.tell() #끝자리 (=파일 용량)을 사이즈로 기록
+#     file.stream.seek(pos) #사이즈 확인 후 포인터 초기화
+#     return size
+
+def allowed_file(filename:str):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_FILE_EXT']
+
+@app.route('/')
+def index():
+    return render_template('upload12.html', filelist=get_filelist())
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    #file = request.form    # 속성, 파일명만 받아옴
+    print(request.files)    # 실제 파일 바이너리를 FileStorage라는 객체 형태로 받아옴
+
+    file = request.files['file']
+    print(file)
+
+    if file.filename == '':
+        return '파일이 올바르게 전송되지 않았습니다.'
+    
+    #app.config['MAX_CONTENT_LENGTH']를 정의했다면 아래 내용을 구현하지 않아도 서버에 정의된 용량 초과로 처리되어 413 상태값을 리턴함
+
+    # max_size = 1 * 1024 * 1024
+    # if get_filesize(file) > max_size:
+    #     return f'첨부 가능한 용량은 {max_size // 1024}KB입니다.'
+
+    if allowed_file(file.filename):
+        file_path = os.path.join('./', app.config['UPLOAD_FOLDER'], file.filename.replace(' ','_'))
+        file.save(file_path)
+        return redirect(url_for('index')) #index 함수가 가지고 있는 라우터 주소로 이동하라는 뜻
+    
+    return '허용되지 않는 파일입니다.'
+
+# @app.route('/delete/<filename>', methods=['GET'])
+@app.route('/delete/<filename>', methods=['POST'])
+def remove_file(filename):
+    file_path = os.path.join('./', app.config['UPLOAD_FOLDER'], filename)
+    try:
+        os.remove(file_path)
+    except FileNotFoundError:
+        return '파일이 존재하지 않거나 이미 삭제되었습니다.'
+    
+    return redirect(url_for('index'))
+
+#http status에 따른 에러 핸들링
+@app.errorhandler(413)
+def too_large(e):
+    size_mb = app.config['MAX_CONTENT_LENGTH'] / (1024 * 1024)
+    return f'업로드한 파일이 너무 큽니다. 최대 {size_mb}MB까지 첨부할 수 있습니다.'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
