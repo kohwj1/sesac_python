367bdc7448ea723683706e78b7a2f51c11b892c5
Arkeil <kohwj1@gmail.com>
2025-07-10 23:21:10 +0900

    add feature (check status in flask template todo list)

--- 5.Project/todolist/flask_template/app.py
+++ 5.Project/todolist/flask_template/app.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
-from common import add_todo, delete_todo, get_all_list
+from common import add_todo, delete_todo, get_all_list, switch_todo
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    print(get_all_list())
     return render_template('todo.html', todolist=get_all_list())
 
 @app.route('/add', methods=['POST'])
 def add_item():
     content = request.form.get('userInput')
     add_todo(content)
+    return redirect(url_for('index'))
+
+@app.route('/update/<idx>', methods=['POST'])
+def update_item(idx):
+    switch_todo(idx)
     return redirect(url_for('index'))
 
 @app.route('/delete/<idx>', methods=['POST'])
