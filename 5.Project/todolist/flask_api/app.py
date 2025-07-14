61de0f3f75e0ad9dc84351f7e092f0d273e00cd5
Arkeil <kohwj1@gmail.com>
2025-07-14 10:12:57 +0900

    added API route, fixed bug in Todo HTML

--- 5.Project/todolist/flask_api/app.py
+++ 5.Project/todolist/flask_api/app.py
@@ -1,34 +1,13 @@
 from flask import Flask, request, jsonify, send_file
 from common import add_todo, delete_todo, get_all_list, switch_todo
+from api_route import api_bp
 
 app = Flask(__name__)
+app.register_blueprint(api_bp, url_prefix='/api')
 
 @app.route('/')
 def index():
     return send_file('static/todo.html')
-
-@app.route('/api/list')
-def get_list():
-    mylist = get_all_list()
-    return jsonify({'data':mylist})
-
-@app.route('/api/add', methods=['POST'])
-def item_add():
-    todo = request.form.get('userInput')
-    add_todo(todo)
-    return jsonify({'result':0,'data':get_all_list()})
-
-
-@app.route('/api/update/<idx>', methods=['PUT'])
-def item_update(idx):
-    switch_todo(idx)
-    return jsonify({'result':0,'data':get_all_list()})
-
-
-@app.route('/api/delete/<idx>', methods=['DELETE'])
-def item_delete(idx):
-    delete_todo(idx)
-    return jsonify({'result':0,'data':get_all_list()})
 
 if __name__ == '__main__':
     app.run(debug=True)
