bdff4df30c930c04a968f6e627ba1435aa50a9ed
Arkeil <kohwj1@gmail.com>
2025-07-16 18:13:42 +0900

    refactored user list functions

--- 5.Project/sesac_crm/database/user_database.py
+++ 5.Project/sesac_crm/database/user_database.py
@@ -20,38 +20,49 @@ def search_users_by_name(name, page, pagesize) -> list | None:
     conn = get_connection()
     cur = conn.cursor()
 
-    cur.execute("SELECT *, COUNT(*) OVER() AS Totalcount FROM users WHERE Name LIKE ? LIMIT ? OFFSET ?", (f'%{name}%', pagesize, off_start))
+    cur.execute("""SELECT *, COUNT(*) OVER() AS Totalcount
+                FROM users
+                WHERE Name LIKE ? LIMIT ? OFFSET ?""", (f'%{name}%', pagesize, off_start))
     return cur.fetchall()
 
-def filter_gender(gender, page, pagesize) -> list | None:
+def search_users_by_gender(gender, page, pagesize) -> list | None:
     off_start = (page - 1) * pagesize
     conn = get_connection()
     cur = conn.cursor()
 
-    cur.execute("SELECT *, COUNT(*) OVER() AS Totalcount FROM users WHERE Gender = ? LIMIT ? OFFSET ?", (gender, pagesize, off_start))
+    cur.execute("""SELECT *, COUNT(*) OVER() AS Totalcount
+                FROM users
+                WHERE Gender = ? LIMIT ? OFFSET ?""", (gender, pagesize, off_start))
     return cur.fetchall()
 
-def get_user_info(userid):
+def search_users_by_name_and_gender(name, gender, page, pagesize) -> list | None:
+    off_start = (page - 1) * pagesize
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""SELECT *, COUNT(*) OVER() AS Totalcount
+                FROM users
+                WHERE Name LIKE ? AND Gender = ?
+                LIMIT ? OFFSET ?""", (f'%{name}%', gender, pagesize, off_start))
+    return cur.fetchall()
+
+def get_user_history(userid):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("""SELECT o.Id AS OrderId, o.OrderAt AS OrderAt, s.Name AS StoreName, s.Id AS StoreId
+                FROM users u
+                JOIN orders o ON u.Id = o.UserId
+                JOIN stores s ON o.StoreId = s.Id 
+                WHERE u.Id = ?
+                ORDER BY OrderAt DESC""", (userid, ))
+    user_history = cur.fetchall()
+    return user_history
+
+def get_user_summary(userid):
     conn = get_connection()
     cur = conn.cursor()
 
     cur.execute("SELECT * FROM users WHERE Id = ?", (userid, ))
-    user_info = cur.fetchone()
+    summary = cur.fetchone()
 
-    def get_user_history(userid):
-        cur.execute("""SELECT o.Id AS OrderId, o.OrderAt AS OrderAt, s.Name AS StoreName, s.Id AS StoreId
-                    FROM users u
-                    JOIN orders o ON u.Id = o.UserId
-                    JOIN stores s ON o.StoreId = s.Id 
-                    WHERE u.Id = ?
-                    ORDER BY OrderAt DESC""", (userid, ))
-        user_history = cur.fetchall()
-        return user_history
-
-    user_history = get_user_history(userid)
-    
-    #로그 확인좀
-    for u in user_history:
-        print(dict(u))
-
-    return {'info':user_info, 'history':user_history}
+    return summary
--- 5.Project/sesac_crm/route/users_route.py
+++ 5.Project/sesac_crm/route/users_route.py
@@ -10,33 +10,32 @@ def user_list():
     page = int(request.args.get('page', default=1))
     pagesize = 20
     pagination_size = 10
+    pages = 0
+    totalpage = 0
     total_count = 0
 
     if name and gender:
-        users = [u for u in userdb.search_users_by_name(name, page, pagesize) if u['Gender'] == gender]
+        users = [u for u in userdb.search_users_by_name_and_gender(name, gender, page, pagesize) if u['Gender'] == gender]
     elif name:
         users = userdb.search_users_by_name(name, page, pagesize)
     elif gender:
-        users = userdb.filter_gender(gender, page, pagesize)
+        users = userdb.search_users_by_gender(gender, page, pagesize)
     else:
         users = userdb.get_all_list(page, pagesize)
 
     if users:
         total_count = users[0]['Totalcount']
-        total_page = (total_count - 1) // pagesize + 1
-
-
+        totalpage = (total_count - 1) // pagesize + 1
         page_start = ((page - 1) // pagination_size) * pagination_size + 1
-        page_end = min(page_start + pagination_size, total_page + 1)
-        print(total_count, total_page, page_start, page_end)
+        page_end = min(page_start + pagination_size, totalpage + 1)
+        print(total_count, totalpage, page_start, page_end)
         pages = [i for i in range(page_start, page_end)]
 
-    return render_template('users.html', users=users, name=name, gender=gender, currentPage=page, pages=pages, totalPage=total_page)
+    return render_template('users.html', users=users, name=name, gender=gender, currentPage=page, pages=pages, totalPage=totalpage)
 
 @user_bp.route('/detail/<userid>')
 def user_detail(userid):
-    user = userdb.get_user_info(userid)
-    userinfo = user['info']
-    orderlist = user['history']
+    userinfo = userdb.get_user_summary(userid)
+    orderlist = userdb.get_user_history(userid)
 
     return render_template('user_detail.html', user=userinfo, orderlist=orderlist)
