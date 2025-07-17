422297e160f5c04802c41bb813d8c6b7dc8c1fc4
Arkeil <kohwj1@gmail.com>
2025-07-17 17:10:43 +0900

    changed return format (list to dict)

--- 5.Project/sesac_crm/common/pagination.py
+++ 5.Project/sesac_crm/common/pagination.py
@@ -1,4 +1,4 @@
-PAGE_SIZE = 20
+PAGE_SIZE = 10
 PAGINATION_SIZE = 10
 TOTAL_COUNT = 0
 
@@ -9,4 +9,4 @@ def pagination(page, selectquery:list):
     page_start = ((page - 1) // PAGINATION_SIZE) * PAGINATION_SIZE + 1
     page_end = min(page_start + PAGINATION_SIZE, total_page + 1)
     # print(total_count, total_page, page_start, page_end)
-    return [i for i in range(page_start, page_end)]
+    return {'pages':[i for i in range(page_start, page_end)], 'totalPage':total_page}
