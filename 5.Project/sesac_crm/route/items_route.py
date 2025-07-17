966199bb8fc11f44172d0e8da3f4cc1686b750e9
Arkeil <kohwj1@gmail.com>
2025-07-17 23:24:58 +0900

    applied pagination function

--- 5.Project/sesac_crm/route/items_route.py
+++ 5.Project/sesac_crm/route/items_route.py
@@ -1,25 +1,20 @@
 from flask import render_template, request, Blueprint
 import database.item as itemdb
+from common.pagination import pagination, PAGE_SIZE
 
 item_bp = Blueprint('items', __name__)
 
 @item_bp.route('/')
 def list():
     page = int(request.args.get('page', default=1))
-    pagesize = 20
-    pagination_size = 10
-    total_count = 0
+    pages = [1]
+    total_page = 1
 
-    items = itemdb.get_all_list(page, pagesize)
+    items = itemdb.get_all_list(page, PAGE_SIZE)
 
     if items:
-        total_count = items[0]['Totalcount']
-        total_page = (total_count - 1) // pagesize + 1
-
-        page_start = ((page - 1) // pagination_size) * pagination_size + 1
-        page_end = min(page_start + pagination_size, total_page + 1)
-        print(total_count, total_page, page_start, page_end)
-        pages = [i for i in range(page_start, page_end)]
+        pages = pagination(page, items)['pages']
+        total_page = pagination(page, items)['totalPage']
 
     return render_template('items.html', items=items, currentPage=page, pages=pages, totalPage=total_page)
 
