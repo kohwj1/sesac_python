5fea9b77e4f0565b4f31b219da9344ac33339dea
Arkeil <kohwj1@gmail.com>
2025-07-17 15:22:51 +0900

    added pagination function

---
+++ 5.Project/sesac_crm/common/pagination.py
@@ -0,0 +1,12 @@
+PAGE_SIZE = 20
+PAGINATION_SIZE = 10
+TOTAL_COUNT = 0
+
+def pagination(page, selectquery:list):
+    total_count = selectquery[0]['Totalcount']
+    total_page = (total_count - 1) // PAGE_SIZE + 1
+
+    page_start = ((page - 1) // PAGINATION_SIZE) * PAGINATION_SIZE + 1
+    page_end = min(page_start + PAGINATION_SIZE, total_page + 1)
+    # print(total_count, total_page, page_start, page_end)
+    return [i for i in range(page_start, page_end)]
