06dba82b64a77e4d81ac95a630f4f1fb7a524853
Arkeil <kohwj1@gmail.com>
2025-07-21 12:21:52 +0900

    fixed bug (fail to render pagination UI)

--- 5.Project/sesac_crm_api/static/js/orderitemList.js
+++ 5.Project/sesac_crm_api/static/js/orderitemList.js
@@ -26,22 +26,5 @@ function displayList() {
             createPagination(parseInt(page), data.lastPage)   
         })
     };
-
-function createPagination(currentPage, lastPage) {
-    const pagination = document.getElementById('pageList')
-    if (currentPage -1 < 1) {
-        pagination.innerHTML = `<li class="pageNum disabled">Prev</li>`
-    } else {
-        pagination.innerHTML = `<li class="pageNum"><a href="?page=${currentPage - 1}">Prev</a></li>`
-    }
-
-    pagination.innerHTML += `<li class="pageNum currentPage">${currentPage}</li>`
-
-    if (currentPage + 1 > lastPage) {
-        pagination.innerHTML += `<li class="pageNum disabled">Next</li>`
-    } else {
-        pagination.innerHTML += `<li class="pageNum"><a href="?page=${currentPage + 1}">Next</a></li>`
-    }
-}
-
+    
 document.addEventListener('DOMContentLoaded', displayList)
