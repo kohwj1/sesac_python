36ceae571474ba8803e45c1e770c98d33e80a0d2
Arkeil <kohwj1@gmail.com>
2025-07-23 12:44:52 +0900

    added feature user create page

---
+++ 5.Project/sesac_crm_api/static/users/js/create.js
@@ -0,0 +1,31 @@
+function createUser() {
+    const userForm = document.getElementById('createForm')
+    let bodyData = new FormData(userForm);
+    console.log(bodyData)
+
+    fetch(`http://localhost:5500/users/api/create`, {
+        method: 'POST',
+        body: bodyData
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            UserId = data.UserId;
+
+            if (data.isCreated) {
+                alert(`고객 등록이 완료되었습니다.\n고객ID: ${UserId}`)
+                location.href = `/users/detail?id=${UserId}`
+            } else {
+                throw new Error;
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+            alert(error)
+        })
+    };
+
+document.getElementById('createForm').addEventListener('submit', e => {
+    e.preventDefault();
+    createUser();
+})
