83c268f05f2230a8715d8c13a63894d659c2ba73
Arkeil <kohwj1@gmail.com>
2025-07-25 10:26:42 +0900

    set limit birthdate for under 14

--- 5.Project/sesac_crm_api/static/users/js/create.js
+++ 5.Project/sesac_crm_api/static/users/js/create.js
@@ -1,4 +1,3 @@
-
 function getAge (birth) {
     const today = new Date();
     const birthday = new Date(Date.parse(birth))
@@ -16,9 +15,14 @@ function createUser() {
     const userForm = document.getElementById('createForm');
     const age = getAge(document.getElementById('Birthdate').value);
     let bodyData = new FormData(userForm);
-    bodyData.append('Age', age);
 
-    console.log(bodyData)
+    if (age < 14) {
+        alert('만 14세 미만은 회원으로 등록할 수 없습니다.');
+        return;
+    }
+
+    bodyData.append('Age', age);
+    // console.log(bodyData)
 
     fetch(`/users/api/create`, {
         method: 'POST',
@@ -39,6 +43,12 @@ function createUser() {
             alert(error)
         })
     };
+
+document.addEventListener('DOMContentLoaded', () => {
+    const today = new Date();
+    const day_limit = `${String(today.getFullYear() - 14)}-${String(today.getMonth() + 1).padStart(2,'0')}-${String(today.getDate()).padStart(2,'0')}`;
+    document.getElementById('Birthdate').setAttribute('max', day_limit);
+})
 
 document.getElementById('createForm').addEventListener('submit', e => {
     e.preventDefault();
