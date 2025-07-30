6988f41d6b79f4fb7a71e5481116e1dd5e5b5165
Arkeil <kohwj1@gmail.com>
2025-07-30 10:04:18 +0900

    move to another repository

--- 5.Project/sesac_crm_api/route/util/pagination.py
+++
@@ -1,9 +0,0 @@
-PAGE_SIZE = 20
-PAGINATION_SIZE = 10
-
-def pagination(selectquery):
-    total_count = selectquery['totalCount']
-    last_page = (total_count - 1) // PAGE_SIZE + 1
-    # print(total_count, last_page)
-    
-    return last_page
