6858e8b257678a9870a2b07cf21c51a8f3f0cdfc
Arkeil <kohwj1@gmail.com>
2025-08-08 13:38:53 +0900

    pdf 자료 db화 연습

--- 7.api/3.openai/24.rag_chatbot/4.pdfloader.py
+++ 7.api/3.openai/24.rag_chatbot/4.pdfloader.py
@@ -10,15 +10,13 @@ from langchain_chroma import Chroma
 
 import os
 
+PERSIST_DIR = 'chroma_db'
+collection_name = 'secure_coding_guide'
 load_dotenv()
 
-pdf_filename = 'DATA/python_guide_for_secure_coding.pdf'
+pdf_filename = 'DATA/secure_coding_guide_for_python.pdf'
 loader = PyPDFLoader(pdf_filename)
 pages = loader.load()
-
-# print(f"총 페이지수: {len(pages)}")
-# print(f"미리보기: {pages[1].page_content[:100]}")
-# print(f"메타데이터: {pages[1].metadata}")
 
 text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
     separator="\n\n", #문서 분할 기준
@@ -30,19 +28,27 @@ texts = text_splitter.split_documents(pages)
 
 embeddings = OpenAIEmbeddings()
 
-
-
-
-
-
-
-store = Chroma.from_documents(texts, embeddings, collection_name="secure_coding_guide", persist_directory='chroma_db')
+if os.path.exists(PERSIST_DIR):
+    store = Chroma(
+        collection_name=collection_name,
+        embedding_function=embeddings,
+        persist_directory=PERSIST_DIR
+    )
+else:
+    store = Chroma.from_documents(texts, embeddings, collection_name=collection_name, persist_directory=PERSIST_DIR)
 
 llm = ChatOpenAI(temperature=0.1)
 template = """
 주어진 문서 내용을 바탕으로 질문에 답변해주세요.
+
 문서 내용: {context}
 질문: {q}
+
+답변을 작성하고, 마지막에 참고한 페이지의 메타데이터를 이용해서 "출처: [source, page]"를 명시해주세요.
+
+출처 표기 예시)
+출처: python_guide_for_secure_coding.pdf, 3페이지
+출처: secure_coding_guide_for_python.pdf, 12페이지
 """
 
 prompt = ChatPromptTemplate.from_template(template)
@@ -55,8 +61,6 @@ res = chain.invoke(q)
 
 print(f'Q: {q}')
 print(f'A: {res}')
-
-
 
 #1. DB 중복 생성하지 않게 분기처리
 #2. 답변에 출처와 페이지를 함께 표시하기
