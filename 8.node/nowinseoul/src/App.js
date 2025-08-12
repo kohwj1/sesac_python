3f343024ab22f08a7ae1e9f432febab5ce3e9d7b
Arkeil <kohwj1@gmail.com>
2025-08-12 16:40:36 +0900

    react intro

---
+++ 8.node/nowinseoul/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+//내 html 페이지를 조각(컴포넌트)
+function App() {
+  return (
+    <div className="App">
+      여기에 내용 입력
+    </div>
+  );
+}
+
+export default App;
---
+++ 8.node/nowinseoul/src/MyApp.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+//내 html 페이지를 조각(컴포넌트)
+function MyApp() {
+  return (
+    <div className="App">
+      두번째 컴포넌트
+    </div>
+  );
+}
+
+export default MyApp;
